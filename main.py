8a5613c02f290025d0d66ae41ffeef28d5253a8c
BoriKui <tokarevnikita170@gmail.com>
2025-10-07 17:26:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import logging
 import random
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import logging
+from telegram.ext import ApplicationBuilder, CommandHandler
+
+
+TOKEN = ""
 
 
 links = [
@@ -10,34 +12,49 @@ links = [
     "https://journal.sovcombank.ru/esg/globalnoe-poteplenie-prichini-i-posledstviya",
     "https://ria.ru/20251006/klimat-2046676016.html",
     "https://rg.ru/2023/11/07/kak-prostoj-chelovek-mozhet-povliiat-na-globalnoe-poteplenie.html"
-    
 ]
 
+logging.basicConfig(level=logging.INFO)
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет! Используй команду /getlink, чтобы получить случайную статью о глобальном потеплении.")
+async def start(update, context):
+    await update.message.reply_text(
+        "Привет! Я бот о глобальном потеплении.\n"
+        "Команды:\n"
+        "/getlink - получить случайную статью\n"
+        "/addlink <ссылка> - добавить новую ссылку\n"
+        "/help - помощь"
+    )
 
+async def help_command(update, context):
+    await update.message.reply_text(
+        "Доступные команды:\n"
+        "/start - приветствие\n"
+        "/getlink - получить случайную статью\n"
+        "/addlink <ссылка> - добавить новую ссылку\n"
+        "/help - показать команды"
+    )
 
-async def get_link(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    link = random.choice(links)
-    await update.message.reply_text(link)
+async def get_link(update, context):
+    if links:
+        link = random.choice(links)
+        await update.message.reply_text(link)
+    else:
+        await update.message.reply_text("Ссылки еще не добавлены.")
 
+async def add_link(update, context):
+    new_link = ' '.join(context.args)
+    if new_link:
+        links.append(new_link)
+        await update.message.reply_text(f"Ссылка добавлена: {new_link}")
+    else:
+        await update.message.reply_text("Пожалуйста, укажите ссылку после команды /addlink.")
 
 def main():
-    
-    TOKEN = "8226728454:AAHp9AMbibrWqyw-0IJrF56qaT8AVJLAerM"
-    
-    
-    logging.basicConfig(level=logging.INFO)
-    
-    
     app = ApplicationBuilder().token(TOKEN).build()
-    
-    
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("help", help_command))
     app.add_handler(CommandHandler("getlink", get_link))
-    
-    
+    app.add_handler(CommandHandler("addlink", add_link))
     app.run_polling()
 
 if __name__ == '__main__':
